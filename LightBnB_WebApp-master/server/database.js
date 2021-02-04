647ddf332b7603c2d2ab45e60ea6fde63c981c59
Your Name <thomas.w.yee@gmail.com>
2021-02-04 15:10:27 -0500

    properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -89,11 +89,44 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
-    .then(res => res.rows);
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+
+  for (const key of options) {
+    if (options.key === 'city' || options.key === 'owner_id') {
+      queryParams.push(`%${options.key}%`);
+      /'where'/ig.test(queryString) ? queryString += `AND ${key} LIKE $${queryParams.length} ` : queryString += `WHERE ${key} LIKE $${queryParams.length} `;
+    } else if (options.key === 'minimum_rating' || options.key === 'minimum_price_per_night') {
+      queryParams.push(`%${options.key}%`);
+      /'where'/ig.test(queryString) ? queryString += `AND ${key} > $${queryParams.length} ` : queryString += `WHERE ${key} > $${queryParams.length} `;
+    } else if (options.key === 'maximum_price_per_night') {
+      queryParams.push(`%${options.key}%`);
+      /'where'/ig.test(queryString) ? queryString += `AND ${key} < $${queryParams.length} ` : queryString += `WHERE ${key} < $${queryParams.length} `;
+    }
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
+  .then(res => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
